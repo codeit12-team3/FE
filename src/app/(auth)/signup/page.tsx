b413ba1085f383ef45af1a1bc7126e52df4dcba4
Kim-HyounSuk <kkoor586@naver.com>
2025-12-04 18:56:51 +0900

    Feat(#11): 회원가입 페이지 작성

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { SingupForm } from '@/components/auth'
+
+export default function Page() {
+  return (
+    <main className="w-full flex-1 flex items-center justify-center max-w-7xl mx-auto px-4 md:px-8">
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-3 w-full">
+        <section className="flex-1 space-y-3 w-full">
+          <h3 className="text-2xl font-semibold text-center">
+            Welcome to Trip Us!
+          </h3>
+          <p className="font-medium text-center">
+            {/* TODO: 멘트 결정나면 바꿈 */}
+            한줄 소개 있으면 좋을 것 같습니다.
+          </p>
+          <Image
+            width={640}
+            height={563}
+            src={'/images/auth/intro.png'}
+            alt={'인트로 이미지'}
+            className="w-full h-auto"
+          />
+        </section>
+        <SingupForm />
+      </div>
+    </main>
+  )
+}
