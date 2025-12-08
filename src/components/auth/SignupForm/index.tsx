055a238820735629c741142200a56dc4e21de70b
Kim-HyounSuk <kkoor586@naver.com>
2025-12-08 12:49:01 +0900

    Chore(#24): 불필요 import 제거

--- src/components/auth/SignupForm/index.tsx
+++ src/components/auth/SignupForm/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { FormProvider, useForm } from 'react-hook-form'
 
