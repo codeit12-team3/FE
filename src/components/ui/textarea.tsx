1e0f0346ef34f7eff95bf2f11a0ef6c76a87a900
Kim-HyounSuk <kkoor586@naver.com>
2025-12-10 17:40:06 +0900

    Design(#33): shadcn/ui 컴포넌트 CSS 수정(Textarea)

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,12 +2,19 @@ import * as React from 'react'
 
 import { cn } from '@/lib/common/index'
 
-function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
+function Textarea({
+  className,
+  disabled,
+  placeholder,
+  ...props
+}: React.ComponentProps<'textarea'>) {
   return (
     <textarea
       data-slot="textarea"
+      placeholder={disabled ? undefined : placeholder}
+      disabled={disabled}
       className={cn(
-        'border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        'placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-xl bg-input px-4 py-3 text-base transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:bg-muted disabled:text-muted-foreground',
         className,
       )}
       {...props}
