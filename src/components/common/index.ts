e126774b4a19b1e8d96242f868365a4fb2a0c409
SH701 <rkrk7636@naver.com>
2025-12-11 11:00:15 +0900

    Feat(#36): 로딩용 스켈레톤 UI 컴포넌트 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 export * from './Button'
 export * from './Input'
+export * from './Skeleton'
 export { default as Header } from './Header'
 export { default as Label } from './Label'
 export { default as Select } from './Select'
--- src/components/post/Skeleton/CommentSkeleton/index.tsx
+++ src/components/post/Skeleton/CommentSkeleton/index.tsx
@@ -1,36 +1,38 @@
+import { Skeleton } from '@/components/common'
+
 export default function CommentItemSkeleton() {
   return (
     <div className="w-4xl mr-auto p-4">
-      <div className="bg-blue-50 rounded-lg p-4 animate-pulse">
-        <div className="h-4 w-28 bg-gray-200 rounded-md mb-3" />
+      <div className=" rounded-lg p-4">
+        <Skeleton className="h-4 w-28  mb-3" />
 
-        <div className="w-full h-20 bg-gray-200 rounded-md" />
+        <Skeleton className="w-full h-20 " />
 
         <div className="flex justify-end gap-2 mt-3">
-          <div className="w-16 h-8 bg-gray-200 rounded-md" />
-          <div className="w-16 h-8 bg-gray-200 rounded-md" />
+          <Skeleton size="sm" />
+          <Skeleton size="sm" />
         </div>
       </div>
-      <div className="flex gap-4 pt-8 animate-pulse">
-        <div className="w-12 h-12 rounded-full bg-gray-200" />
+      <div className="flex gap-4 pt-8 ">
+        <Skeleton size="circle" className="w-12 h-12" />
 
         <div className="flex-1 space-y-2">
           <div className="flex items-center justify-between">
             <div className="space-y-2">
-              <div className="h-4 w-24 bg-gray-200 rounded-md" />
-              <div className="h-3 w-20 bg-gray-200 rounded-md" />
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="h-3 w-20" />
             </div>
 
-            <div className="w-4 h-4 bg-gray-200 rounded" />
+            <Skeleton size="circle" className="w-4 h-4 " />
           </div>
 
           <div className="space-y-2 mt-2">
-            <div className="h-4 w-2/3 bg-gray-200 rounded-md" />
+            <Skeleton className="w-2/3 " />
           </div>
 
           <div className="flex gap-4 mt-2">
-            <div className="h-3 w-16 bg-gray-200 rounded-md" />
-            <div className="h-3 w-20 bg-gray-200 rounded-md" />
+            <Skeleton size="sm" />
+            <Skeleton size="sm" />
           </div>
         </div>
       </div>
