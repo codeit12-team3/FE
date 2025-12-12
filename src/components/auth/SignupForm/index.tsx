cb964898f9b59e99c26517790871dc22c2541b79
Kim-HyounSuk <kkoor586@naver.com>
2025-12-12 10:36:02 +0900

    Chore(#37): 미사용 import 제거

--- src/components/auth/SignupForm/index.tsx
+++ src/components/auth/SignupForm/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { FormProvider, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
