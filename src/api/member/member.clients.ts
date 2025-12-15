3e57894218db8128badbbd25345f5f47b29e42a7
Kim-HyounSuk <kkoor586@naver.com>
2025-12-15 11:11:51 +0900

    Chore(#45): api 호출 클라이언트 엔드포인트에 버전 명시(member 관련)

--- src/api/member/member.clients.ts
+++ src/api/member/member.clients.ts
@@ -15,7 +15,7 @@ import { axios } from '../common'
  * @param 닉네임
  */
 export const checkNickname = async (nickname: string) => {
-  const res = await axios.get('/v1/members/nickname/check', {
+  const res = await axios.get<ApiResponse<null>>('/v1/members/nickname/check', {
     params: { nickname },
   })
 
@@ -24,7 +24,7 @@ export const checkNickname = async (nickname: string) => {
 
 // 내정보 조회
 export const getMyProfile = async (): Promise<MyProfile> => {
-  const res = await axios.get<ApiResponse<MyProfile>>('/members/me')
+  const res = await axios.get<ApiResponse<MyProfile>>('/v1/members/me')
 
   if (!res.data.success) {
     throw new Error(res.data.data?.message || '프로필을 불러오지 못했습니다')
@@ -34,7 +34,7 @@ export const getMyProfile = async (): Promise<MyProfile> => {
 }
 // 프로필 수정할때
 export const updateMyProfile = async (data: UpdateMyProfileReq) => {
-  const res = await axios.patch<ApiResponse<null>>('/members/me', data)
+  const res = await axios.patch<ApiResponse<null>>('/v1/members/me', data)
   return res.data
 }
 
