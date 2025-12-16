daad6ef12b4f60c99382a5a224071bcefd311a64
Kim-HyounSuk <kkoor586@naver.com>
2025-12-16 11:00:56 +0900

    Design(#49): Auth 관련 페이지 intro 이미지 사이즈 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: Props) {
     <main className="w-full flex-1 flex items-center justify-center max-w-7xl mx-auto px-4 md:px-8">
       <div className="flex flex-col lg:flex-row items-center justify-center gap-3 w-full">
         <section className="flex-1 space-y-3 w-full">
-          <h3 className="text-2xl font-semibold text-center">
+          <h3 className="text-2xl font-semibold text-center text-primary">
             Welcome to Trip Us!
           </h3>
           <p className="font-medium text-center">
@@ -17,11 +17,11 @@ export default function Layout({ children }: Props) {
             한줄 소개 있으면 좋을 것 같습니다.
           </p>
           <Image
-            width={640}
-            height={563}
+            width={431}
+            height={218}
             src={'/images/auth/intro.png'}
             alt={'함께 여행하는 모습이 담긴 인트로 일러스트 이미지'}
-            className="w-full h-auto"
+            className="w-full h-auto max-w-[431px] mx-auto"
           />
         </section>
         {children}
