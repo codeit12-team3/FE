0a63ea23f065d078d2a56c74a637bba54d7cc317
swp91 <seongwoo4477@gmail.com>
2025-12-19 16:16:25 +0900

    test(#72):BioField 컴포넌트 글자수제한 추가 및 테스트코드 추가

--- src/components/member/edit/BioField/index.tsx
+++ src/components/member/edit/BioField/index.tsx
@@ -1,6 +1,22 @@
+'use client'
+
+import { useFormContext, useWatch } from 'react-hook-form'
+
 import FormTextarea from '@/components/form/FormTextarea'
+import { ProfileEditFormData } from '@/types/member/schema'
+
+const MAX_LENGTH = 100
 
 export default function BioField() {
+  const { control } = useFormContext<ProfileEditFormData>()
+
+  const introduction = useWatch({
+    control,
+    name: 'introduction',
+  })
+
+  const currentLength = introduction?.length || 0
+
   return (
     <div className="mt-6 flex flex-col gap-3">
       <FormTextarea
@@ -8,6 +24,8 @@ export default function BioField() {
         name="introduction"
         placeholder="자기소개를 입력해주세요"
         className="resize-none min-h-30"
+        maxLength={MAX_LENGTH}
+        rightContent={`${currentLength}/${MAX_LENGTH}`}
       />
     </div>
   )
