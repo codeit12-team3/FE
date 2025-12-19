211bfbb43271c5ae175993561a7443191587caed
Kim-HyounSuk <kkoor586@naver.com>
2025-12-19 15:07:52 +0900

    Design(#60): shadcn/ui 컴포넌트 스타일 수정(변경된 디자인 적용: Button)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@/lib/common/index'
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center cursor-pointer gap-2 whitespace-nowrap font-semibold transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-blue-500 focus-visible:ring-blue-300 focus-visible:ring-[3px] aria-invalid:ring-red aria-invalid:border-red",
+  "inline-flex items-center justify-center cursor-pointer gap-2 whitespace-nowrap font-semibold transition-all disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-blue-500 focus-visible:ring-blue-500/40 focus-visible:ring-[3px] aria-invalid:ring-red aria-invalid:border-red",
   {
     variants: {
       variant: {
@@ -26,11 +26,7 @@ const buttonVariants = cva(
           'hover:bg-gray-100 hover:text-blue-600',
           'active:bg-gray-200 active:text-blue-600 border-blue-600',
         ],
-        ghost: [
-          'text-gray-500',
-          'hover:bg-blue-200 text-gray-600',
-          'active:bg-blue-300 text-gray-600',
-        ],
+        ghost: ['text-gray-500', 'hover:text-gray-600', 'active:text-gray-600'],
         link: [
           'text-blue-500 underline-offset-4',
           'hover:text-blue-600 hover:underline',
@@ -38,7 +34,7 @@ const buttonVariants = cva(
         ],
       },
       size: {
-        'md': 'h-10 rounded-xl px-6 py-2 text-sm has-[>svg]:px-3',
+        'md': 'h-10 rounded-xl px-6 py-[10px] text-sm has-[>svg]:px-3',
         'lg': 'h-14 rounded-2xl px-7.5 py-4 text-base has-[>svg]:px-4',
         'icon': 'size-9',
         'icon-sm': 'size-8',
