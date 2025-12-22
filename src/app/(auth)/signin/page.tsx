165473f1e61cd43c7268a252f63f89819e004593
Kim-HyounSuk <kkoor586@naver.com>
2025-12-22 10:48:05 +0900

    Design(#60): 변경된 디자인으로 스타일 수정(로그인)

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,29 +5,31 @@ import { OAuthButton } from '@/components/auth/form'
 
 export default function Page() {
   return (
-    <section className="px-4 md:px-14 py-6 md:py-11 rounded-[40px] bg-white flex-1 w-full space-y-4">
-      <div className="space-y-[35px]">
-        <h2 className="text-2xl font-semibold text-center">로그인</h2>
+    <section className="px-10 py-8 rounded-4xl bg-white flex-1 w-full space-y-6 border border-gray-200">
+      <h2 className="text-xl font-semibold text-center">로그인</h2>
+      <div className="space-y-3">
         <SigninForm />
+        <p className="text-center text-xs font-medium">
+          트립어스가 처음이신가요?{' '}
+          <Link
+            className="text-blue-500 underline underline-offset-1"
+            href={'/signup'}
+          >
+            회원가입
+          </Link>
+        </p>
       </div>
-      <p className="text-center text-sm">
-        트립어스가 처음이신가요?{' '}
-        <Link
-          className="text-main underline underline-offset-1"
-          href={'/signup'}
-        >
-          회원가입
-        </Link>
-      </p>
-      <div className="flex items-center w-full">
-        <div className="grow border-t bg-text-base" />
-        <span className="px-[23px]">또는</span>
-        <div className="grow border-t bg-text-base" />
-      </div>
-      <div className="flex items-center justify-center gap-[30px]">
-        <OAuthButton provider="google" size="lg" />
-        <OAuthButton provider="kakao" size="lg" />
-        <OAuthButton provider="naver" size="lg" />
+      <div className="space-y-3">
+        <div className="flex items-center w-full">
+          <div className="grow border-t border-gray-300" />
+          <span className="px-2 text-gray-500 text-xs">또는</span>
+          <div className="grow border-t border-gray-300" />
+        </div>
+        <div className="flex items-center justify-center gap-6">
+          <OAuthButton provider="google" />
+          <OAuthButton provider="kakao" />
+          <OAuthButton provider="naver" />
+        </div>
       </div>
     </section>
   )
