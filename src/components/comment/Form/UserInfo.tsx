340848441c23f36d1000b011f71ad99eea71f81f
Kim-HyounSuk <kkoor586@naver.com>
2025-12-22 10:42:50 +0900

    Refactor(#60): user.session undefined 시 오류 수정(getImageUrl 함수 래핑)

--- src/components/comment/Form/UserInfo.tsx
+++ src/components/comment/Form/UserInfo.tsx
@@ -1,6 +1,8 @@
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 
+import { getImageUrl } from '@/lib/common'
+
 export default function UserInfo() {
   const { data: session } = useSession()
   // 임시 처리
@@ -10,7 +12,12 @@ export default function UserInfo() {
   return (
     <div className="w-full flex items-center gap-2 justify-start">
       <div className="w-6 aspect-square relative rounded-full overflow-hidden">
-        <Image src={user.image} alt="user profile" fill sizes="24px" />
+        <Image
+          src={getImageUrl(user.image)}
+          alt="user profile"
+          fill
+          sizes="24px"
+        />
       </div>
       <p className="font-semibold text-sm text-gray-900">{user.nickname}</p>
     </div>
