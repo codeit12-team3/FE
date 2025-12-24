9c09a09eb825d0eacf0be059f80c6cf7b6e817f6
Kim-HyounSuk <kkoor586@naver.com>
2025-12-24 17:35:58 +0900

    Chore(#79): 불필요 import 제거

--- src/components/member/ProfileEdit/index.tsx
+++ src/components/member/ProfileEdit/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Loader2 } from 'lucide-react'
 import { useEffect } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 
