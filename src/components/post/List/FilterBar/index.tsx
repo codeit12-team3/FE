c6b1592557b4991407a501caa2f2d5d81064a12f
SH701 <rkrk7636@naver.com>
2025-12-24 16:50:58 +0900

    Feat(#86): 필터바 반응형 UI 추가

--- src/components/post/List/FilterBar/index.tsx
+++ src/components/post/List/FilterBar/index.tsx
@@ -171,74 +171,152 @@ export default function FilterBar({
   const TRIGGER = 'w-22 text-sm text-gray-800 font-medium'
 
   return (
-    <div className="max-w-7xl mx-auto p-4 flex gap-2 justify-between">
-      <div className="flex gap-2 ">
-        {/* 국가 */}
-        <FilterSelect
-          value={filters.nation}
-          options={NATION_ENUM_OPTIONS}
-          placeholder="국가"
-          onChange={(value) =>
-            applyImmediately({
-              nation: value === 'ALL' ? '' : value,
-            })
-          }
-          className={TRIGGER}
-        />
-
-        {/* 나이 */}
-        <FilterSelect
-          value={filters.ageType}
-          options={AGE_OPTIONS}
-          placeholder="나이"
-          onChange={(value) =>
-            applyImmediately({
-              ageType: value === 'ALL' ? undefined : (value as AgeType),
-            })
-          }
-          className={TRIGGER}
-        />
-
-        {/* 성별 */}
-        <FilterSelect
-          value={filters.gender}
-          options={GENDER_OPTIONS}
-          placeholder="성별"
-          onChange={(value) =>
-            applyImmediately({
-              gender: value === 'ALL' ? undefined : (value as GenderType),
-            })
-          }
-          className={TRIGGER}
-          includeAllOption={false}
-        />
-        {/* 날짜 */}
-        <FilterDatePicker
-          date={filters.date}
-          onApply={(date) => applyImmediately({ date })}
-          triggerClassName="w-auto min-w-22 text-sm text-gray-800 font-medium"
-        />
-      </div>
-
-      {/* 검색 */}
-      <div className="flex-1 relative max-w-[456px]">
-        <InputGroup className="h-10">
-          <InputGroupInput
-            placeholder="검색어를 입력해주세요"
-            value={keyword}
-            onChange={(e) => setKeyword(e.target.value)}
+    <div className="max-w-7xl mx-auto p-4">
+      {/* 데스크톱 레이아웃 */}
+      <div className="hidden xl:flex gap-2 justify-between">
+        <div className="flex gap-2">
+          {/* 국가 */}
+          <FilterSelect
+            value={filters.nation}
+            options={NATION_ENUM_OPTIONS}
+            placeholder="국가"
+            onChange={(value) =>
+              applyImmediately({
+                nation: value === 'ALL' ? '' : value,
+              })
+            }
+            className={TRIGGER}
           />
-          <IconSearch className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500 pointer-events-none" />
-        </InputGroup>
+
+          {/* 나이 */}
+          <FilterSelect
+            value={filters.ageType}
+            options={AGE_OPTIONS}
+            placeholder="나이"
+            onChange={(value) =>
+              applyImmediately({
+                ageType: value === 'ALL' ? undefined : (value as AgeType),
+              })
+            }
+            className={TRIGGER}
+          />
+
+          {/* 성별 */}
+          <FilterSelect
+            value={filters.gender}
+            options={GENDER_OPTIONS}
+            placeholder="성별"
+            onChange={(value) =>
+              applyImmediately({
+                gender: value === 'ALL' ? undefined : (value as GenderType),
+              })
+            }
+            className={TRIGGER}
+            includeAllOption={false}
+          />
+          {/* 날짜 */}
+          <FilterDatePicker
+            date={filters.date}
+            onApply={(date) => applyImmediately({ date })}
+            triggerClassName="w-auto min-w-22 text-sm text-gray-800 font-medium"
+          />
+        </div>
+
+        {/* 검색 */}
+        <div className="flex-1 relative w-full">
+          <InputGroup className="h-10">
+            <InputGroupInput
+              placeholder="검색어를 입력해주세요"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+            <IconSearch className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500 pointer-events-none" />
+          </InputGroup>
+        </div>
+
+        <Button
+          size="md"
+          className="gap-2 w-36"
+          onClick={() => router.push('/posts/add')}
+        >
+          동행 구하기
+        </Button>
       </div>
 
-      <Button
-        size="md"
-        className="gap-2 w-36"
-        onClick={() => router.push('/posts/add')}
-      >
-        동행 구하기
-      </Button>
+      {/* 모바일 레이아웃 */}
+      <div className="flex xl:hidden flex-col gap-3">
+        {/* 상단: 검색과 동행 구하기 버튼 */}
+        <div className="flex gap-2 justify-between">
+          <div className="flex-1 relative max-w-[332px]">
+            <InputGroup className="h-10">
+              <InputGroupInput
+                placeholder="검색어를 입력해주세요"
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
+              />
+              <IconSearch className="absolute right-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500 pointer-events-none" />
+            </InputGroup>
+          </div>
+
+          <Button
+            size="md"
+            className="gap-2 whitespace-nowrap"
+            onClick={() => router.push('/posts/add')}
+          >
+            동행 구하기
+          </Button>
+        </div>
+
+        {/* 하단: 필터링 조건 */}
+        <div className="flex gap-2 overflow-x-auto">
+          {/* 국가 */}
+          <FilterSelect
+            value={filters.nation}
+            options={NATION_ENUM_OPTIONS}
+            placeholder="국가"
+            onChange={(value) =>
+              applyImmediately({
+                nation: value === 'ALL' ? '' : value,
+              })
+            }
+            className={TRIGGER}
+          />
+
+          {/* 나이 */}
+          <FilterSelect
+            value={filters.ageType}
+            options={AGE_OPTIONS}
+            placeholder="나이"
+            onChange={(value) =>
+              applyImmediately({
+                ageType: value === 'ALL' ? undefined : (value as AgeType),
+              })
+            }
+            className={TRIGGER}
+          />
+
+          {/* 성별 */}
+          <FilterSelect
+            value={filters.gender}
+            options={GENDER_OPTIONS}
+            placeholder="성별"
+            onChange={(value) =>
+              applyImmediately({
+                gender: value === 'ALL' ? undefined : (value as GenderType),
+              })
+            }
+            className={TRIGGER}
+            includeAllOption={false}
+          />
+
+          {/* 날짜 */}
+          <FilterDatePicker
+            date={filters.date}
+            onApply={(date) => applyImmediately({ date })}
+            triggerClassName="w-auto min-w-22 text-sm text-gray-800 font-medium"
+          />
+        </div>
+      </div>
     </div>
   )
 }
