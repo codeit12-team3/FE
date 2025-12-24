19730b707b1eff5121e063bd54e3e95d55fe2bdf
Kim-HyounSuk <kkoor586@naver.com>
2025-12-24 17:22:59 +0900

    Chore(#79): react-query 관련 상수 추가(GC_TIME, STALE_TIME)

---
+++ src/constants/common/react-query.const.ts
@@ -0,0 +1,12 @@
+export const GC_TIME = {
+  DEFAULT: 1000 * 60 * 60 * 10, // 10시간
+  HOUR: 1000 * 60 * 60, // 1시간
+  HALF: 1000 * 60 * 30, // 30분
+  MINUTE: 1000 * 60, // 1분
+}
+
+export const STALE_TIME = {
+  DEFAULT: 1000 * 60 * 60, // 1시간
+  HALF: 1000 * 60 * 30, // 30분
+  MINUTE: 1000 * 60, // 1분
+}
