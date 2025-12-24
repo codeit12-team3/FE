124e87566f7d19155fc8edc42fbeae198e3c1818
Kim-HyounSuk <kkoor586@naver.com>
2025-12-24 17:26:48 +0900

    Feat(#79): 마이페이지의 신청한 동행 관련 내부 컴포넌트 구현

---
+++ src/components/member/SentCompanion/SentList/index.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { Loader } from 'lucide-react'
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+import { useInfiniteGetSentCompanions } from '@/api/companions'
+import { CompanionState } from '@/types/companions'
+
+import EmptyCard from '../../EmptyCard'
+import { useTabs } from '../../Tabs'
+import SentCard from '../SentCard'
+import SentCardSkeleton from '../SentCardSkeleton'
+
+export default function SentList() {
+  const { activeTab } = useTabs<CompanionState>()
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useInfiniteGetSentCompanions(activeTab)
+
+  const { ref, inView } = useInView({ threshold: 0 })
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage()
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage])
+
+  const companions = data?.pages.flatMap((page) => page.content) ?? []
+
+  const isInitialLoading = isLoading && companions.length === 0
+
+  return (
+    <div className="flex flex-col gap-4">
+      {isInitialLoading &&
+        [1, 2, 3].map((value) => (
+          <SentCardSkeleton key={`received-card-skeleton-${value}`} />
+        ))}
+
+      {!isInitialLoading && companions.length === 0 && (
+        <EmptyCard comment="아직 받은 동행 정보가 없어요." />
+      )}
+
+      {companions.map((item, idx) => (
+        <SentCard
+          key={`${companions}-${item.myGuestCompanionResponse.companionId}`}
+          data={item}
+          idx={idx}
+        />
+      ))}
+
+      <div ref={ref} className="h-20 flex items-center justify-center">
+        {isFetchingNextPage && <Loader className="size-6 animate-spin" />}
+      </div>
+    </div>
+  )
+}
