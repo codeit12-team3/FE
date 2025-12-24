8a37d8444985d3287cbc3b9fc847e4f017f3805f
Kim-HyounSuk <kkoor586@naver.com>
2025-12-24 17:13:48 +0900

    Refactor(#79): 동행 요청 관련 타입 추가

---
+++ src/types/companions/type.ts
@@ -0,0 +1,76 @@
+import { NationCode } from '@/constants/posts'
+
+export type CompanionState = 'PENDING' | 'APPROVE' | 'DENIED'
+
+export interface ApplyCompanionReq {
+  postId: string
+  applyMessage: string
+}
+
+export interface ApplyCompanionRes {
+  postId: number
+  status: Extract<CompanionState, 'PENDING'>
+}
+
+export interface UpdateCompanionReq {
+  companionId: string
+  status: Exclude<CompanionState, 'PENDING'>
+}
+
+export interface ReceivedCompanionRes {
+  content: ReceivedCompanionContent[]
+  page: {
+    size: number
+    number: number
+    totalElements: number
+    totalPages: number
+  }
+}
+
+export interface SentCompanionRes {
+  content: SentCompanionContent[]
+  page: {
+    size: number
+    number: number
+    totalElements: number
+    totalPages: number
+  }
+}
+
+export interface ReceivedCompanionContent {
+  guestCompanionResponse: {
+    companionId: string
+    memberId: string
+    nickname: string
+    status: CompanionState
+    applyMessage: string
+  }
+  postResponse: {
+    id: string
+    title: string
+    thumbnail: string | null
+    tags: string[]
+    nation: NationCode
+    region: string
+    startDate: string
+    endDate: string
+  }
+}
+
+export interface SentCompanionContent {
+  myGuestCompanionResponse: {
+    companionId: string
+    status: CompanionState
+    applyMessage: string
+  }
+  postResponse: {
+    id: string
+    title: string
+    thumbnail: string | null
+    tags: string[]
+    nation: NationCode
+    region: string
+    startDate: string
+    endDate: string
+  }
+}
