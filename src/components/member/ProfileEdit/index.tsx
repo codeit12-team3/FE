94a567a37b3ddf24b83a5916ae7cc9143e47c257
Kim-HyounSuk <kkoor586@naver.com>
2025-12-24 17:30:27 +0900

    Refactor(#79): 프로필 수정 페이지와 내부로직 분리

---
+++ src/components/member/ProfileEdit/index.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Loader2 } from 'lucide-react'
+import { useEffect } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+
+import { useMyProfileQuery } from '@/api/member'
+import { ProfileEditFormData, profileEditSchema } from '@/types/member'
+
+import { ProfileEditForm, ProfileImageEdit } from '../edit'
+
+export default function ProfileEdit() {
+  const { data } = useMyProfileQuery()
+
+  const methods = useForm<ProfileEditFormData>({
+    resolver: zodResolver(profileEditSchema),
+    defaultValues: {
+      email: '',
+      image: '',
+      nickname: '',
+      birth: '',
+      gender: '',
+      mbti: '',
+      lodgingStyle: '',
+      tripStyle: '',
+      introduction: '',
+    },
+  })
+
+  useEffect(() => {
+    if (data) {
+      methods.reset({
+        email: data.email || '',
+        image: data.image || '',
+        nickname: data.nickname || '',
+        birth: data.birth || '',
+        gender: data.gender,
+        mbti: data.mbti || '',
+        lodgingStyle: data.lodgingStyle || 'ALL',
+        tripStyle: data.tripStyle || 'ALL',
+        introduction: data.introduction || '',
+      })
+    }
+  }, [data, methods])
+
+  return (
+    <FormProvider {...methods}>
+      <div className="flex justify-center mb-6">
+        <ProfileImageEdit />
+      </div>
+      <div className="w-inherit">
+        <ProfileEditForm />
+      </div>
+    </FormProvider>
+  )
+}
