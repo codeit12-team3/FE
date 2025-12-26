c9bdc91c941deec48dd251f9d9d0c11b68d14704
hyeopsang <dbtkdguq3@gmail.com>
2025-12-26 18:28:04 +0900

    Feat(#81): 커스텀 Toast 컴포넌트 도입 - 기존 global.css에 정의된 toast 스타일 제거 - shadcn toast 컴포넌트 제거 - 디자인 시안에 따른 커스텀 Toast 컴포넌트 구현

---
+++ src/components/common/Toast/index.tsx
@@ -0,0 +1,53 @@
+import type { ReactNode } from 'react'
+import { toast as sonnerToast, Toaster as SonnerToaster } from 'sonner'
+
+import CheckCircle from '@/assets/svgr/check-circle.svg'
+import ErrorCircle from '@/assets/svgr/error-circle.svg'
+
+export type ToastType = 'default' | 'success' | 'error'
+
+const TOAST_ICON_MAP: Record<ToastType, ReactNode> = {
+  default: null,
+  success: <CheckCircle className="h-6 w-6 text-green-400" />,
+  error: <ErrorCircle className="h-6 w-6 text-red-400" />,
+}
+
+export const renderToast = (
+  message: string,
+  type: ToastType,
+  duration: number,
+) => {
+  sonnerToast.custom(
+    (id) => (
+      <div
+        onClick={() => sonnerToast.dismiss(id)}
+        className="flex items-center justify-center gap-2 rounded-[12px] bg-black/60 w-[222px] h-11 text-white text-xs font-bold"
+      >
+        {TOAST_ICON_MAP[type]}
+        <span className="truncate">{message}</span>
+      </div>
+    ),
+    { duration },
+  )
+}
+export const toast = {
+  success: (message: string, duration = 2000) =>
+    renderToast(message, 'success', duration),
+  error: (message: string, duration = 2000) =>
+    renderToast(message, 'error', duration),
+  info: (message: string, duration = 2000) =>
+    renderToast(message, 'default', duration),
+}
+
+export const Toaster = () => {
+  return (
+    <SonnerToaster
+      position="bottom-center"
+      toastOptions={{
+        unstyled: true,
+      }}
+      offset={40}
+      gap={8}
+    />
+  )
+}
