838fa3bcb15382757b12b7101814e320178afe75
Kim-HyounSuk <kkoor586@naver.com>
2025-12-29 05:11:42 +0900

    Design(#91): Auth 관련 페이지 반응형 ui 적용

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -4,7 +4,7 @@ import { SignupForm } from '@/components/auth'
 
 export default function Page() {
   return (
-    <section className="px-10 py-8 rounded-4xl bg-white flex-1 w-full space-y-3 border border-gray-200">
+    <section className="max-w-[440px] px-4 md:px-10 py-8 rounded-4xl bg-white flex-1 w-full space-y-3 border border-gray-200">
       <div className="space-y-6">
         <h2 className="text-xl font-semibold text-center">회원가입</h2>
         <SignupForm />
