3d9b0019e6f75ff680dc67b52c1f1325a03deb42
Kim-HyounSuk <kkoor586@naver.com>
2025-12-29 09:34:26 +0900

    Design(#91): 공통 헤더 반응형 ui 적용

--- src/components/common/Header/UserMenu/index.tsx
+++ src/components/common/Header/UserMenu/index.tsx
@@ -22,7 +22,7 @@ export default function UserMenu() {
   return session ? (
     <Popover>
       <PopoverTrigger asChild>
-        <button className="flex size-10 shrink-0 border border-gray-300 rounded-full items-center justify-center cursor-pointer">
+        <button className="flex size-8 md:size-10 shrink-0 border border-gray-300 rounded-full items-center justify-center cursor-pointer">
           <Image
             src={getImageUrl(session?.user.image, true)}
             alt="프로필 이미지"
@@ -59,7 +59,10 @@ export default function UserMenu() {
       </PopoverContent>
     </Popover>
   ) : (
-    <Link href={'/signin'} className="text-sm font-semibold text-gray-500">
+    <Link
+      href={'/signin'}
+      className="text-xs md:text-sm font-semibold text-gray-500 hover:text-gray-700 active:text-gray-700"
+    >
       로그인
     </Link>
   )
--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -4,44 +4,48 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { cn } from '@/lib/common'
+
 import UserMenu from './UserMenu'
 
 export default function Header() {
   const pathname = usePathname()
 
   return (
-    <header className=" border-b border-gray-200 bg-white">
-      <div className="max-w-7xl mx-auto px-8 py-3 ">
+    <header className="border-b border-gray-200 bg-white h-[52px] md:h-[68px]">
+      <div className="max-w-7xl mx-auto px-4 py-2 md:px-6 md:py-3 ">
         <div className="flex items-center justify-between">
-          <div className="flex items-center gap-10 ">
+          <div className="flex items-center gap-3 md:gap-8 ">
             <Link href="/">
               <Image
                 src="/images/logo.png"
-                alt="Trip us"
-                width={110}
-                height={39}
-                style={{ width: '100%', height: '100%' }}
+                alt="Trip us 메인 로고"
+                width={90}
+                height={32}
+                className="w-[72px] h-[25px] md:w-[90px] md:h-8"
                 priority
               />
             </Link>
-            <nav className="flex gap-8">
+            <nav className="flex gap-2">
               <Link
                 href="/"
-                className={`text-base transition-colors ${
+                className={cn(
+                  'transition-colors text-xs font-semibold px-5 py-2.5 md:text-sm',
                   pathname === '/'
-                    ? 'text-blue-500 '
-                    : 'text-gray-500 hover:text-blue-500'
-                }`}
+                    ? 'text-blue-500'
+                    : 'text-gray-500 hover:text-gray-700 active:text-gray-700',
+                )}
               >
                 게시판
               </Link>
               <Link
-                href="/chatting"
-                className={`transition-colors ${
+                href="/"
+                className={cn(
+                  'transition-colors text-xs font-semibold px-5 py-2.5 md:text-sm',
                   pathname === '/chatting'
-                    ? 'text-blue-500 font-semibold'
-                    : 'text-gray-500 hover:text-blue-500'
-                }`}
+                    ? 'text-blue-500'
+                    : 'text-gray-500 hover:text-gray-700 active:text-gray-700',
+                )}
               >
                 채팅방
               </Link>
