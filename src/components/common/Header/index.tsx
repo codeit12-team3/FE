f675692535f40d69f0dca5312706325f5bb60b29
Kim-HyounSuk <kkoor586@naver.com>
2025-12-31 01:45:17 +0900

    Feat(#99): 알림 버튼 추가

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -26,11 +26,11 @@ export default function Header() {
                 priority
               />
             </Link>
-            <nav className="flex gap-2">
+            <nav className="flex md:gap-2">
               <Link
                 href="/"
                 className={cn(
-                  'transition-colors text-xs font-semibold md:text-sm',
+                  'shrink-0 transition-colors text-xs font-semibold md:text-sm',
                   pathname === '/'
                     ? 'text-blue-500'
                     : 'text-gray-500 hover:text-gray-700 active:text-gray-700',
@@ -43,7 +43,7 @@ export default function Header() {
               <Link
                 href="/"
                 className={cn(
-                  'transition-colors text-xs font-semibold md:text-sm',
+                  'shrink-0 transition-colors text-xs font-semibold md:text-sm',
                   pathname === '/chatting'
                     ? 'text-blue-500'
                     : 'text-gray-500 hover:text-gray-700 active:text-gray-700',
